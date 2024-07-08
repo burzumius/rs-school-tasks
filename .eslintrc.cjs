845da257343f42e42bef47dba536549ee8b6a275
Ramil <crouzch1@gmail.com>
2024-07-08 19:50:09 +0500

    fix: eslint & ts config fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	root: true,
-	env: { browser: true, es2020: true },
+	env: { browser: true, es2020: true, },
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
@@ -10,17 +10,17 @@ module.exports = {
 	ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'public'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
 	settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+		react: {
+			version: 'detect',
+		},
+	},
 	plugins: ['react-refresh', 'react-compiler', 'prettier'],
 	rules: {
 		'react-refresh/only-export-components': [
